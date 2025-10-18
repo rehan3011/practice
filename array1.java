94ac779667b27c039a070335252fd543bebf6e90
rehan3011 <rehan.d.3011@gmail.com>
2025-10-18 13:02:44 +0530

    simple array

---
+++ array1.java
@@ -0,0 +1,11 @@
+public class array1 {
+    public static void main(String[] args){
+        int arr[]= new int[10];
+        for(int i=0; i<10; i++){
+            arr[i]=i;
+        }
+        for(int i=0; i<10; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
