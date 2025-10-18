f6e79d0225418174baf0e627fdfd851b88b03a14
rehan3011 <rehan.d.3011@gmail.com>
2025-10-18 23:40:04 +0530

    Array List

---
+++ array3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class array3 {
+    public static void main(String[] args){
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    }
+}
