2845e9a675d468c49f78d978bb2b2d8e4c4d5856
Rehan <rehan.d.3011@gmail.com>
2025-10-21 18:32:14 +0530

    Time: 5 ms (64.46%), Space: 44 MB (62.63%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int rev=0;
+        while(x>0){
+            int lastd=x%10;
+            rev=rev*10+lastd;
+            x=x/10;
+        }
+        if(original==rev)
+        return true;
+        else
+        return false;
+    }
+}
