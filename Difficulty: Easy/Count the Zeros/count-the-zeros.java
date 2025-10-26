137754ed3c3c416e6a841ce8b66df25684e2f39a
Rehan <rehan.d.3011@gmail.com>
2025-10-26 18:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    int countZeroes(int[] arr) {
+        int c=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==0)
+            c++;
+        }
+        return c;
+    }
+}
