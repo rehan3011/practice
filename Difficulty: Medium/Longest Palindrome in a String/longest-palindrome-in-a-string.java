224416c3ce72864ab340b4939da74192d48fcf5a
Rehan <rehan.d.3011@gmail.com>
2026-01-20 21:20:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    static String longestPalindrome(String s) {
+        String longest="";
+        for(int i=0; i<s.length(); i++){
+            String odd = expand(s, i, i);
+            String even = expand(s, i, i+1);
+            if(odd.length()>longest.length())
+            longest=odd;
+            if(even.length()>longest.length())
+            longest=even;
+        }
+        return longest;
+    }
+    static String expand(String s, int left, int right){
+        while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return s.substring(left+1, right);
+    }
+}
