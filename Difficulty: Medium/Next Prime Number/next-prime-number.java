4c22339947a4322c13e70dfa140140be37268d2a
Rehan <rehan.d.3011@gmail.com>
2026-03-09 21:05:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    public static int nextPrime(int n) {
+    
+    int i,j;
+    int flag;
+    for( i=n+1; i<500; i++){
+          flag=1;
+        for( j=2; j<i; j++){
+            if(i%j==0){
+                flag=0;
+                break;
+            }
+        }
+        if(flag==1)
+        return i;
+    }
+    
+    return -1;
+    }
+}
